a685c1e2ce0735c67ba151a922403b60807283d9
Mart Kol <martin.kolesnik@tktk.ee>
2024-11-03 13:33:43 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Security
+
+string firstName, lastName;
+
+firstName = "Harry";
+lastName = "Potter";
+
+NewMethod(firstName, lastName);
+
+static void NewMethod(string firstName, string lastName)
+{
+    Console.WriteLine($"Hello",firstName + lastName!);
+}
